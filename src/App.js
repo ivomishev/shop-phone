41766474b830acb5e3406411df96d6d7ee63464c
Ivo Mishev <ivo.kr.mishev@gmail.com>
2019-03-19 10:43:07 +0200

    configure routing and navBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Switch, Route  } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
@@ -14,12 +14,13 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar/>
-        <ProductList/>
-        <Details />
-        <Cart />
-        <Default />
-        
-
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+  
       </React.Fragment>
     );
   }
